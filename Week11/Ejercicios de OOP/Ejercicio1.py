123fdb5dbaebe3a1d115482edbf72f7ecab77372
Rudy Villalobos <Rudyturbocross@gmail.com>
2025-08-07 16:45:46 -0600

    07/08UPDATE

--- Week11/Ejercicios de OOP/Ejercicio1.py
+++ Week11/Ejercicios de OOP/Ejercicio1.py
@@ -1,8 +1,13 @@
 class Circle:
-    radius = 3
+    radius = []
+
+
+    def __init__(self, diameter):
+        self.radius = 3.1416*diameter**2
+    
 
     def get_area(self):
         print(f"The Radius is {self.radius}")
 
-result = Circle()
+result = Circle(5)
 result.get_area()
--- Week11/Ejercicios de OOP/Ejercicio2.py
+++ Week11/Ejercicios de OOP/Ejercicio2.py
@@ -1,34 +1,50 @@
+class Person:
+
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+        
+
+
 class Bus:
 
 
     def __init__(self, max_passengers):
         self.max_passengers = max_passengers
-        self.Person = 0
+        self.passengers = []
 
 
 
-    def board_passenger(self):   
-        if self.Person < self.max_passengers:
-            self.Person += 1
-            print(f"\ntheres 1 passengers getting ✅ On the Bus, there {self.Person} in total")
-            if self.Person == self.max_passengers:
+    def board_passenger(self, Person):   
+        if len(self.passengers) < self.max_passengers:
+            self.passengers.append(Person) 
+            print(f"\n{Person} is getting ON ✅ the Bus")
+            if self.passengers == self.max_passengers:
                 print("\n---the Bus is Completely Full---")
                 
 
-    def exit_one_passen(self):
-        if self.Person > 0:
-            self.Person -= 1
-            print(f"\ntheres 1 passengers getting ❌ Off the Bus, there {self.Person} passengers now in the bus")
-            if self.Person == 0:
+    def exit_one_passen(self, Person):
+        if Person in self.passengers:
+            self.passengers.remove(Person)
+            print(f"\n{Person} is getting OFF ❌ the Bus")
+            if self.passengers == 0:
                 print("\nThe Bus is Empty...")   
+
+P1 = Person("Mario")
+P2 = Person("Sara")
+P3 = Person("Carlos")
+P4 = Person("Sofia")
 
 bus_ten = Bus(10)
 
-bus_ten.board_passenger()
-bus_ten.board_passenger()
-bus_ten.exit_one_passen()
-bus_ten.board_passenger()
-bus_ten.exit_one_passen()
-bus_ten.board_passenger()
-bus_ten.board_passenger()
-bus_ten.board_passenger()
+bus_ten.board_passenger(P1)
+bus_ten.board_passenger(P2)
+bus_ten.exit_one_passen(P1)
+bus_ten.board_passenger(P3)
+bus_ten.exit_one_passen(P2)
+bus_ten.board_passenger(P1)
+bus_ten.board_passenger(P2)
+bus_ten.board_passenger(P4)
