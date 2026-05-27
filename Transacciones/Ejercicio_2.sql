26f4e2eb1f45e8cc3ab83b358ccb4d5485512a56
rudyturbocross <rudyturbocross@gmail.com>
2026-05-27 13:18:13 -0600

    27/5

--- Transacciones/Ejercicio_2.sql
+++ Transacciones/Ejercicio_2.sql
@@ -1,21 +1,32 @@
 Do $$
 
 DECLARE 
-    V_Stock_Available INTEGER;
+    V_Stock_Product_1 INTEGER;
+    V_Stock_Product_2 INTEGER;
+    V_Product_Order_Quantity_1 INTEGER := 2;
+    V_Product_Order_Quantity_2 INTEGER := 3;
     V_User_On_DB VARCHAR(40);
 
 BEGIN
 
-    SELECT Inventory INTO V_Stock_Available
+    SELECT Inventory INTO V_Stock_Product_1
     FROM Products
-    WHERE ID = 'P003';
+    WHERE ID = 'P003'; --Product 1
+
+    SELECT Quantuty INTO V_Stock_Product_2
+    FROM Products
+    WHERE ID = 'P001'; --Product 2
 
     SELECT Name INTO V_User_On_DB
     FROM Users
     WHERE ID = 'U005';
 
-    If V_Stock_Available IS NULL OR V_Stock_Available < 2 THEN
-    RAISE EXCEPTION '--Stock Not Available--';
+    If V_Stock_Product_1 IS NULL OR V_Stock_Product_1 < V_Stock_Product_1 THEN
+    RAISE EXCEPTION '-- P003 Stock Not Available--'; --Product 1
+    END IF;
+
+    If V_Stock_Product_2 IS NULL OR V_Stock_Product_2 < V_Stock_Product_2 THEN
+    RAISE EXCEPTION '-- P001 Stock Not Available--'; --Product 2
     END IF;
 
     If V_User_On_DB IS NULL THEN
@@ -26,12 +37,18 @@ BEGIN
         VALUES ('B006', 'U005');
 
     INSERT INTO Bill_Details (ID, Bill_ID, Product_ID, Quantity, Unit_Price)
-        VALUES ('D006', 'B006', 'P003', 2, 1);
+        VALUES ('D007', 'B006', 'P003', 2, 1); --Product 1
 
+    INSERT INTO Bill_Details (ID, Bill_ID, Product_ID, Quantity, Unit_Price)
+        VALUES ('D006', 'B006', 'P001', 3, 2); --Product 2
 
     UPDATE Products
     SET Inventory = Inventory - 2
-    WHERE ID = 'P003';
+    WHERE ID = 'P003'; --Product 1
+
+    UPDATE Products
+    SET Inventory = Inventory - 3
+    WHERE ID = 'P001'; --Product 2
 
     RAISE NOTICE 'Transaction Complete';
 
--- Transacciones/Ejercicio_3.sql
+++ Transacciones/Ejercicio_3.sql
@@ -1,41 +1,46 @@
 DO $$
 
 DECLARE
-    V_Bill_Details_ID VARCHAR(40) := 'D003';
-    V_Products_return INTEGER;
+    V_Bill_ID VARCHAR(40) := 'B003';
     V_Product_ID VARCHAR(40);
-    V_ID_Bills VARCHAR(40);
+    V_Products_return INTEGER;
 
 BEGIN
-    SELECT Bill_ID INTO V_ID_Bills
-    FROM Bill_Details
-    WHERE ID = V_Bill_Details_ID;
-
-    SELECT Product_ID INTO V_Product_ID
-    FROM Bill_Details
-    WHERE ID = V_Bill_Details_ID;
-
-    SELECT Quantity INTO V_Products_return
-    FROM Bill_Details
-    WHERE ID = V_Bill_Details_ID;
-
-
     IF NOT EXISTS (
     SELECT 1
-    FROM Bill_Details 
-    WHERE ID = V_Bill_Details_ID
-    ) THEN 
+    FROM Bills
+    WHERE ID = V_Bill_ID
+    ) THEN
 
     RAISE EXCEPTION 'THE BILL DONT EXISTS';
     END IF;
-    
+
+    IF EXISTS (
+    SELECT 1
+    FROM Bills
+    WHERE ID = V_Bill_ID
+    AND Status = 'Returned'
+    ) THEN
+
+    RAISE EXCEPTION 'THE BILL IS ALL READY RETURNED';
+    END IF;
+
+    SELECT Product_ID INTO V_Product_ID
+    FROM Bill_Details
+    WHERE Bill_ID = V_Bill_ID;
+
+    SELECT Quantity INTO V_Products_return
+    FROM Bill_Details
+    WHERE Bill_ID = V_Bill_ID;
+
+
     UPDATE Products 
     SET Inventory = Inventory + V_Products_return
     WHERE ID = V_Product_ID;
 
     UPDATE Bills
     SET Status = 'Returned'
-    WHERE ID = V_ID_Bills;
+    WHERE ID = V_Bill_ID;
 
     RAISE NOTICE 'Transaction Complete';
 
